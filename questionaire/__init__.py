d29fe1822c99423a2413cfd4d30b9107399af37f
Johan <johan+gk@webmaster.co.za>
2019-04-24 08:19:11 +0200

    set DB URI and Undoing stuff

--- questionaire/__init__.py
+++ questionaire/__init__.py
@@ -19,22 +19,24 @@ loginmanager.login_message_category = 'info'
 mail = Mail()
 
 
-def app_config(app):
-    app.config['SECRET_KEY'] = 'd31b4f37bf2b1a8be9f93107c1d27ad083e92b4ed6416866b5487be73765a2b5'
-
-    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-    app.config['MAIL_PORT'] = '587'
-    app.config['MAIL_USE_TLS'] = 'True'
-
-    app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+# def app_config(app):
+#     app.config['SECRET_KEY'] = 'd31b4f37bf2b1a8be9f93107c1d27ad083e92b4ed6416866b5487be73765a2b5'
+#
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#
+#     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+#     app.config['MAIL_PORT'] = '587'
+#     app.config['MAIL_USE_TLS'] = 'True'
+#
+#     app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+#     app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
 
 def create_app(config_class):
     app = Flask(__name__)
-    # app.config.from_object(config_class)
+    app.config.from_object(config_class)
 
-    app_config(app)
+    # app_config(app)
 
     db.init_app(app)
     bcrypt.init_app(app)
